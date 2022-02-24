a9e4ebcb08f4d7f3406c755206d72ad900b00488
wendril <wendrild.c.n@gmail.com>
2022-02-24 14:43:00 -0300

    Requisito 3 - Feito

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Tryunfo</h1>
+      <>
+        <div>
+          <h1>Tryunfo</h1>
+        </div>
         <div>
           <Form />
+          <Card />
         </div>
-      </div>
+      </>
     );
   }
 }
