b082a43b3501396f1b1e5caaf71901d4393b14ca
wendril <wendrild.c.n@gmail.com>
2022-02-28 13:31:07 -0300

    Requisito 5 - Feito

--- src/App.js
+++ src/App.js
@@ -5,8 +5,20 @@ import Card from './components/Card';
 class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '',
+      cardAttr2: '',
+      cardAttr3: '',
+      cardImage: '',
+      cardRare: 'normal',
+      cardTrunfo: false,
+      hasTrunfo: false,
+      isSaveButtonDisabled: true,
+    };
     this.onInputChange = this.onInputChange.bind(this);
+    this.validateSaveButton = this.validateSaveButton.bind(this);
   }
 
   onInputChange({ target }) {
@@ -14,7 +26,33 @@ class App extends Component {
     const value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({
       [name]: value,
-    });
+    }, () => this.validateSaveButton());
+  }
+
+  validateSaveButton() {
+    const { state } = this;
+    const max = 90;
+    const min = 0;
+    const sum = 210;
+
+    if (
+      state.cardName !== ''
+    && state.cardImage !== ''
+    && state.cardDescription !== ''
+    && parseInt(state.cardAttr1, 10) >= min
+    && parseInt(state.cardAttr2, 10) >= min
+    && parseInt(state.cardAttr3, 10) >= min
+    && parseInt(state.cardAttr1, 10) <= max
+    && parseInt(state.cardAttr2, 10) <= max
+    && parseInt(state.cardAttr3, 10) <= max
+    && parseInt(state.cardAttr1, 10)
+    + parseInt(state.cardAttr2, 10)
+    + parseInt(state.cardAttr3, 10) <= sum
+    ) {
+      this.setState({ isSaveButtonDisabled: false });
+    } else {
+      this.setState({ isSaveButtonDisabled: true });
+    }
   }
 
   render() {
@@ -27,6 +65,7 @@ class App extends Component {
         <div>
           <Form
             onInputChange={ onInputChange }
+            isSaveButtonDisabled={ state.isSaveButtonDisabled }
           />
           <Card
             cardName={ state.cardName }
@@ -38,9 +77,6 @@ class App extends Component {
             cardRare={ state.cardRare }
             cardTrunfo={ state.cardTrunfo }
             hasTrunfo={ state.hasTrunfo }
-            isSaveButtonDisabled={ state.isSaveButtonDisabled }
-            onInputChange={ this.onInputChange }
-            onSaveButtonClick={ this.onSaveButtonClick }
           />
         </div>
       </>
